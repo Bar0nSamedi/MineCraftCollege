72f2aec98e0ce09a7a03d3346ffb4688d8818324
Matt <Marcel.b.mueller@stud.h-da.de>
2015-06-09 11:57:53 +0200

    fixed intro Kappa Kappa

--- stuck/src/stuck/Stuck.java
+++ stuck/src/stuck/Stuck.java
@@ -1,10 +1,6 @@
 /***
- * Excerpted from "Learn to Program with Minecraft Plugins",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/ahmine for more book information.
+ * stolen from Polen
+ * Kappa
 ***/
 package stuck;
 
